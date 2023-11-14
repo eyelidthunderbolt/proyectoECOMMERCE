ecf89bef0148bc7db0bec7b383f3bc9d105b2370
Mario Acuña <m.acuna@salesianospizarrales.educamos.com>
2023-11-14 23:36:16 +0100

    Crear los componentes base

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
+import { CentralComponent } from './central/central.component';
+import { LatIzqComponent } from './components/lat-izq/lat-izq.component';
+import { LatDerComponent } from './components/lat-der/lat-der.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadComponent
+    HeadComponent,
+    CentralComponent,
+    LatIzqComponent,
+    LatDerComponent,
+    CategoriasComponent,
+    CarritoComponent,
+    ListaProductosComponent,
+    ProductoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/lat-izq/lat-izq.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lat-izq',
+  templateUrl: './lat-izq.component.html',
+  styleUrls: ['./lat-izq.component.css']
+})
+export class LatIzqComponent {
+
+}
