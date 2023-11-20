c4210b3bee7757a75dc9d21da07d9c32f4c4d4a1
eyelidthunderbolt <peripheryandlight@gmail.com>
2023-11-20 19:54:36 +0100

    Arreglar error en productos controlador

--- server/controllers/productos.controlador.js
+++ server/controllers/productos.controlador.js
@@ -1,31 +1,40 @@
-const controladorCarrito = {};
+const controladorProducto = {};
 const { request } = require('express');
-const carritos = require('../models/carritos');
-const carritosModels = require('../models/carritos');
+const productos = require('../models/productos');
+const productosModels = require('../models/productos');
 
-controladorCarrito.mostrarCarritos = async (req,res) => {
-    const leerCarritos = await carritosModels.find();
-    res.json(leerCarritos);
+controladorProducto.mostrarProductos = async (req,res) => {
+    const leerProductos = await productosModels.find();
+    res.json(leerProductos);
 }
 
 controladorProducto.crearProducto = async (req,res) => {
     const nuevoProducto = new productosModels(req.body);
     await nuevoProducto.save();
 
-    res.json("Carrito Almacenado");
+    res.json("Producto Almacenado");
 }
 
-controladorProducto.mostrarCarrito = async (req,res) => {
+controladorProducto.mostrarProducto = async (req,res) => {
 
-    const buscarCarrito = await carritosModels.findById(req.params.id);
-    res.json(buscarCarrito)
+    const buscarProducto = await productosModels.findById(req.params.id);
+    res.json(buscarProducto)
 }
 
+controladorProducto.editarProducto = async (req,res) => {
+    const { id } = req.params;
+    const productoAEditar = {
 
+        //Definir campos del producto
+    };
 
-controladorCarrito.borrarCarrito = async (req,res) => {
+    await productosModels.findByIdAndUpdate(id,{ $set: productoAEditar }, {new:true});
+    res.json('Producto Actualizado');
+}
+
+controladorProducto.borrarProducto = async (req,res) => {
     await productos.findByIdAndDelete(req.params.id);
-    res.json('Carrito Eliminado')
+    res.json('Producto Eliminado')
 }
 
 module.exports = controladorProducto;
