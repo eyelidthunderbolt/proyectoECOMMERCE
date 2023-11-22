4da467ba7604d75754d70f8caa87995d78a98873
Asanler <antoniocrodrigo2002@gmail.com>
2023-11-22 19:48:33 +0100

    IMPLEMENTADA FUNCIONALIDAD PARCIAL CATEGORIAS

--- frontend/src/app/components/categorias/categorias.component.ts
+++ frontend/src/app/components/categorias/categorias.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductosService } from 'src/app/services/productos.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-categorias',
@@ -6,10 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./categorias.component.css']
 })
 export class CategoriasComponent {
+  productos: Producto[] = [];
 
+  constructor(public productoService: ProductosService, private dataService : DataService) {}
 
-  mostrarCategoria(cad: string){
-    
-}
+  recogerProductos(categoria:string) { //se obtiene la lista de productos de la base de datos
+    return this.productoService.mostrarProductos()
+      .subscribe(res => {
+        this.productos = res as Producto[];
+        console.log(res)
+        const productosCategoria = this.productos.filter((Producto:Producto)=> Producto.categoria === categoria); //Guardamos los porductos filtrados por la categoria que se pasa en el tipo String en el metodo
+      });
+  }
 }
 
