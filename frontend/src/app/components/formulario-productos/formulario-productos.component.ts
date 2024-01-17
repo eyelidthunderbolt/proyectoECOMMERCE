a03f12f69a3ae1135998ae78d79081fc2d104f39
eyelidthunderbolt <peripheryandlight@gmail.com>
2024-01-17 18:05:33 +0100

    revertir cambios centimos

--- frontend/src/app/components/formulario-productos/formulario-productos.component.ts
+++ frontend/src/app/components/formulario-productos/formulario-productos.component.ts
@@ -4,7 +4,7 @@ import { Producto } from 'src/app/models/producto';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
-declare var M : any;
+declare var M: any;
 
 @Component({
   selector: 'app-formulario-productos',
@@ -13,9 +13,9 @@ declare var M : any;
 })
 export class FormularioProductosComponent {
 
-  public categorias : Array<string> = ["Camiseta", "Pantalon", "Zapatillas", "Relojes"];
+  public categorias: Array<string> = ["Camiseta", "Pantalon", "Zapatillas", "Relojes"];
 
-  constructor(public productosService: ProductosService){}
+  constructor(public productosService: ProductosService) { }
 
   ngAfterViewInit() {
     // Script de inicialización de Materialize CSS
@@ -23,66 +23,64 @@ export class FormularioProductosComponent {
     M.FormSelect.init(elems, {});
   }
 
-  anhadirProducto(form : NgForm){
-    
-    if(form.value.nombre == "" || form.value.descripcion == "" || form.value.categorias == "" || (form.value.precio == "" || isNaN(form.value.precio))  || (form.value.stock == "" || isNaN(form.value.stock)) ){
+  anhadirProducto(form: NgForm) {
 
-      M.toast({html:"Faltan campos requeridos / Valores incorrectos"})
-    }else{
-      form.value.precio=this.convertirACentimos(form.value.precio)
+    if (form.value.nombre == "" || form.value.descripcion == "" || form.value.categorias == "" || (form.value.precio == "" || isNaN(form.value.precio)) || (form.value.stock == "" || isNaN(form.value.stock))) {
 
-    
-    
-   if(form.value._id){
-      form.value.precio=this.convertirACentimos(form.value.precio)
+      M.toast({ html: "Faltan campos requeridos / Valores incorrectos" })
+
+
+    }
+
+    else if (form.value._id) {
       this.productosService.actualizarProducto(form.value)
-      .subscribe(res => {
-        
-        console.log(res);
-        this.resetForm();
-        M.toast({html : "Producto Actualizado"})
-        this.obtenerProductos()
+        .subscribe(res => {
+
+          console.log(res);
+          this.resetForm();
+          M.toast({ html: "Producto Actualizado" })
+          this.obtenerProductos()
 
 
-      })
+        })
     }
 
     else {
       form.value._id = null;
       this.productosService.crearProducto(form.value)
-      .subscribe(res =>{
-        console.log(res)
-        this.resetForm(form)
-        M.toast({html : "Producto Almacenado"})
-        this.obtenerProductos();
-      })
+        .subscribe(res => {
+          console.log(res)
+          this.resetForm(form)
+          M.toast({ html: "Producto Almacenado" })
+          this.obtenerProductos();
+        })
     }
-    }
+
   }
 
-    obtenerProductos(){
+  obtenerProductos() {
 
-      this.productosService.mostrarProductos()
+    this.productosService.mostrarProductos()
       .subscribe(res => {
         this.productosService.productos = res as Producto[]; // mete en el array de empleados definido en Services la res que trae los empleados de la base de datos
         console.log(res)
       })
-    }
+  }
 
-    editarProducto(producto: Producto){
-      this.productosService.productoSeleccionado = producto;
-    }
+  editarProducto(producto: Producto) {
+    this.productosService.productoSeleccionado = producto;
+  }
 
-  resetForm(form?:NgForm){
-    if(form){
+  resetForm(form?: NgForm) {
+    if (form) {
       form.reset();
       this.productosService.productoSeleccionado = new Producto;
     }
   }
 
-  convertirACentimos(precio:number){
-    var precioFinal=0
-    precioFinal = precio*100
+  convertirACentimos(precio: number) {
+    var precioFinal = 0
+    precioFinal = precio * 100
     return precioFinal
   }
 
